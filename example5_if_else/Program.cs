c87a855c47c67ef46a7d4e191b67568df06aef5e
Yaros Lap <yaroslap86@gmail.com>
2023-01-25 13:32:12 +0300

    ex5_if_else

---
+++ example5_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "даша")
+{
+    Console.WriteLine("Ура, это же ДАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
